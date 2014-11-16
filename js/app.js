c08e07ac17e26cee6da3d924db0889adca57cf15
Izzy <izzbot@mindspring.com>
2014-11-16 01:54:38 -0800

    v2.

--- js/app.js
+++ js/app.js
@@ -9,22 +9,24 @@ var commentsUrl = 'https://api.parse.com/1/classes/comments';
 
 
 // Build our module
-angular.module('Commenter', ['ui.bootstrap'])
+angular.module('CommenterApp', ['ui.bootstrap'])
     .config(function($httpProvider) {
         $httpProvider.defaults.headers.common['X-Parse-Application-Id'] = 'cXXmzY2CipGaqiydD6qmgZRraLIZyfRWoPABR7yI';
         $httpProvider.defaults.headers.common['X-Parse-REST-API-Key'] = 'MoxE3HBQRWIPP1sRcKsR7CjM2Qs9EjaWUbHaoxL7';
     })
 
     // Build a new controller
-    .controller('commenterController', function($scope, $http) {
+    .controller('CommentsController', function($scope, $http) {
 
         // Initialize a new Comment object
         $scope.newComment = {score: 0};
 
-
-        $scope.refreshComments = function() {
+		// Allows us to grab the latest comments from Parse
+        $scope.refreshComments = function(predicate) {
             $scope.loading = true;
-            $http.get(commentsUrl + '?where={"done": false}')
+
+			$scope.predicate = predicate;
+            $http.get(commentsUrl)
                 .success(function(data) {
                     $scope.comments = data.results;
                 })
@@ -35,9 +37,6 @@ angular.module('Commenter', ['ui.bootstrap'])
                    $scope.loading = false;
                 });
         };
-
-        // Refresh upon load
-        $scope.refreshComments();
 
         // Adds a comment to the list
         $scope.addComment = function() {
@@ -58,7 +57,7 @@ angular.module('Commenter', ['ui.bootstrap'])
 
         // Updates a comment
         $scope.updateComment = function(comment) {
-            $scope.loading = true;
+          $scope.loading = true;
           $http.put(commentsUrl + '/' + comment.objectId, comment)
               .success(function() {
                 // Don't need to to much upon a success
@@ -74,22 +73,30 @@ angular.module('Commenter', ['ui.bootstrap'])
 
         // Changes the score of a comment
         $scope.changeScore = function(comment, amount) {
-            $scope.loading = true;
-            $http.put(commentsUrl + '/' + comment.objectId, {
-                votes: {
-                    __op: 'Increment',
-                    amount: amount
-                }
-            })
-                .success(function(responseData) {
-                    comment.score = responseData.score;
-                })
-                .error(function(err) {
-                    $scope.errorMessage = err;
-                })
-                .finally(function() {
-                   $scope.loading = false;
-                });
+            
+            // Don't let them make the score less than 0
+            if (comment.score + amount >=0) {
+	            $scope.loading = true;
+	            $http.put(commentsUrl + '/' + comment.objectId, {
+	                score: {
+	                    __op: 'Increment',
+	                    amount: amount
+	                }
+	            })
+	                .success(function(responseData) {
+	                    comment.score = responseData.score;
+	                })
+	                .error(function(err) {
+	                    $scope.errorMessage = err;
+	                })
+	                .finally(function() {
+	                   $scope.loading = false;
+	                });            	
+            }
 
         };
+
+
+        // Refresh upon load
+        $scope.refreshComments('-score');
     });
